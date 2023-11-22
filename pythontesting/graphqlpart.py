32bd3cb1a73011818726f238763aedc01008acc6
sami <msami.bese22seecs@student.nust.edu.pk>
2023-11-23 02:47:36 +0500

    testing

--- pythontesting/graphqlpart.py
+++ pythontesting/graphqlpart.py
@@ -1,9 +1,35 @@
 import uvicorn
 from fastapi import FastAPI
+import strawberry
+from strawberry.asgi import GraphQL
+from typing import List
+
+
+@strawberry.type
+class movie:
+    title:str
+    director:str
+
+
+@strawberry.type
+class query1:
+    @strawberry.field
+    def movies(self)->List[movie]:
+        movies_data=[
+            movie(title="Sami", director="nolan"),
+        ]
+        return movies_data
+schema=strawberry.Schema(query=query1)
 
 app = FastAPI()
 
+
 @app.get("/")
-def home():
-    return "Welcome Home"
+def index():
+    return "Hello"
+
+
+app.add_route("/grapql",GraphQL(schema,debug=True))
+
+
 
