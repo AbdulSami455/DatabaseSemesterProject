16db7ad17c33caece28de9a591c3ea0f9492e4a6
sami <msami.bese22seecs@student.nust.edu.pk>
2023-11-23 01:29:07 +0500

    testing

--- pythontesting/graphqlpart.py
+++ pythontesting/graphqlpart.py
@@ -1,14 +1,15 @@
 import uvicorn
-from  fastapi import FastAPI
+from fastapi import FastAPI
 
-app=FastAPI(
-    title="Graphql API on Python",
-    description="FastApi",
+app = FastAPI(
+    title="GraphQL API on Python",
+    description="FastAPI",
     version="1.3.4"
 )
 
 @app.get("/")
-def Home():
+def home():
     return "Welcome Home"
 
-uvicorn.run("graphqlpart:app",host="localhost",port=3400,reload=True)
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=3400, reload=True)
