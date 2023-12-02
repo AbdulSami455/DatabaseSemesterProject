728a92ccf9a7fe1a33aebd0c49351e335db31493
sami <msami.bese22seecs@student.nust.edu.pk>
2023-12-02 14:26:44 +0500

    testing

--- pythontesting/app.py
+++ pythontesting/app.py
@@ -2,7 +2,7 @@
 
 import requests
 import pymysql
-
+from datetime import datetime
 #Importing Database Connection
 from database import connection
 
@@ -40,17 +40,20 @@ def create_user(username, password):
         if conn:
             conn.close()
 '''
-# Call the function
+
+
+#Username and Password Function, brought from Firebase
 def username(username,password):
     if username =='sami' and password =='sami1234':
         return 1
     else:
         return 0
 
-def tablefill(a):
+#FIll table according to Username and Password
+def tablefill():
     try:
         conn = connection()
-
+        cursor=conn.cursor()
         cursor.execute("INSERT INTO Wallet (Walletid) VALUES (1);")
     # Insert into Users table
         cursor.execute("INSERT INTO Users (Userid, Walletid) VALUES (1, 1);")
@@ -58,3 +61,54 @@ def tablefill(a):
     except pymysql.Error as e:
         print(f"Error: {e}")
 
+def enteramount():
+    # Get the amount as input
+    amount_str = input("Enter Amount you want to Deposit: ")
+
+
+    try:
+        amount = float(amount_str)
+    except ValueError:
+        print("Invalid amount. Please enter a valid numeric value.")
+        return
+
+    # Get the current date and time
+    current_datetime = datetime.now()
+
+    # Establish a database connection
+    try:
+        conn = connection()
+        cursor = conn.cursor()
+
+        # Update the total amount in the Wallet table
+        cursor.execute(f"UPDATE Wallet SET totalamount = {amount} WHERE Walletid = 1;")
+
+        #  datetime as a string in 'YYYY-MM-DD HH:MM:SS' format
+        formatted_datetime = current_datetime.strftime('%Y-%m-%d %H:%M:%S')
+
+        # Insert a transaction record
+        cursor.execute(f"INSERT INTO transactions (walletid, amount, transactiontime) VALUES (1, {amount}, '{formatted_datetime}');")
+
+        # Commit the changes
+        conn.commit()
+
+        print("Transaction completed successfully.")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+
+
+us=input("Enter Username : ")
+ps=input("Enter Password : ")
+if username(us,ps):
+    print("Correct Username")
+    #tablefill()
+    enteramount()
+else:
+    print("Wrong Password")
